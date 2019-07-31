936e324bf3e49ee8dcb96e3644bf2a0043b7360a
Victoria Wilson <victoriawilson.production@gmail.com>
2019-07-31 12:16:49 -0500

    created header and footer .ejs files

--- EasyRider/routes/index.js
+++ EasyRider/routes/index.js
@@ -4,7 +4,7 @@ var bootstrap = require('bootstrap');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EasyRider' });
 });
 
 /* CSS through Bootstrap */
