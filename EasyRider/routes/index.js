5cac614cf25f96250d5369628f73a03aa63baa82
Victoria Wilson <victoriawilson.production@gmail.com>
2019-07-31 11:38:48 -0500

    Node_module stuff

--- EasyRider/routes/index.js
+++ EasyRider/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var bootstrap = require('bootstrap');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* CSS through Bootstrap */
+route.get('/', function(req, res, next){
+	res.sendFile('../node_modules/bootstrap/dist/css/bootstrap.css')
+});
+
 
 module.exports = router;
