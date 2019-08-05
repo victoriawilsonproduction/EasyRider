d84d251c34f3b5f2b8009111471dc6ebe0fc5c6e
Victoria Wilson <victoriawilson.production@gmail.com>
2019-08-05 14:35:48 -0500

    Got all the links working! +other stuff

--- EasyRider/routes/index.js
+++ EasyRider/routes/index.js
@@ -6,6 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'EasyRider' });
 });
 
+// GET EasyRider page
+router.get('/EasyRider', function(req, res, next) {
+  res.render('EasyRider', { title: 'EasyRider' });
+});
+
+// GET About page
+router.get('/About', function(req, res, next) {
+  res.render('aboutme', { title: 'About Us' });
+});
+
+
 /* CSS through Bootstrap */
 router.get('/bootstrap', function(req, res, next){
 	res.sendFile('../node_modules/bootstrap/dist/css/bootstrap.css')
