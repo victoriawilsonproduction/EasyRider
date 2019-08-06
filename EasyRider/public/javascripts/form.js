d6c09360785e1e01f3fc36e07a0ccfc993eca3a7
Victoria Wilson <victoriawilson.production@gmail.com>
2019-08-06 13:45:51 -0500

    Add new button works

---
+++ EasyRider/public/javascripts/form.js
@@ -0,0 +1,11 @@
+var addSection = $('#addItem:last').clone();
+var itemId = 0;
+
+$('#addButton').click(function() {
+  itemId++;
+  var newDiv = $('#addItem').clone().appendTo('#addedSection');
+
+  newDiv.find('input').val('');
+});
+
+
